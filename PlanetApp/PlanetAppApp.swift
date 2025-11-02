9ed19705f032a97686027114436c64c836b8823a
Dylan CAETANO <dcaetano8@icloud.com>
2025-11-02 21:51:24 +0100

    Initial Commit

---
+++ PlanetApp/PlanetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetAppApp.swift
+//  PlanetApp
+//
+//  Created by Dylan caetano on 02/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlanetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
